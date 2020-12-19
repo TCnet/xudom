5e9602dabfc04f9499cfb1dbaf7f439d5798b58c
Hxmusic <hxmusic@foxmail.com>
2020-12-19 17:36:35 +0800

    update config

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Aodrusa
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # this casue can't read ActiveStorage 
+    config.autoloader = :classic
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
